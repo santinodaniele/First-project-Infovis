9f012c04a80e7ce5127736a7474bad183a004e20
Daniele Santino <danielesantino@MBPdiDaeSantino.lan>
2020-05-03 19:46:14 +0200

    Update project
    Aggiunta funzione che permette di cambiare il raggio dei cerchi cliccando sugli assi.

--- script.js
+++ script.js
@@ -13,35 +13,49 @@ var svg = d3.select("#mioGrafico")
 
 // Aggiungo l'asse delle X
 var x = d3.scaleLinear()
-	.domain([0, 10000])
+	.domain([0, 100])
 	.range([ 0, width ]);
+
 svg.append("g")
 	.attr("transform", "translate(0," + height + ")")
-	.call(d3.axisBottom(x));
+	.attr("class","xAxis")
+	.call(d3.axisBottom(x).ticks(20))
+
+svg.append("text")
+	.attr("text-anchor", "end")
+	.attr("x", width)
+	.attr("y", height + margin.top + 20)
+	.text("X")
+	.attr("fill","black");
+
+svg.append("text")
+	.attr("text-anchor", "end")
+	.attr("transform", "rotate(-90)")
+	.attr("y", -margin.left+60)
+	.attr("x", -margin.top+40)
+	.text("Y")
+	.attr("fill","black");
 
 // Aggiungo l'asse delle Y
 var y = d3.scaleLinear()
-	.domain([35, 90])
+	.domain([0, 100])
 	.range([ height, 0]);
 svg.append("g")
-	.call(d3.axisLeft(y));
-
-var z = d3.scaleLinear()
-	.domain([200000, 1310000000])
-	.range([ 1, 40]);
+	.attr("class","yAxis")
+	.call(d3.axisLeft(y).ticks(20));
 
 // Creo una variabile che mi dice il nome della bolla quando ci passo sopara
-var tooltip = d3.select("#mioGrafico")
+var nomeBolla = d3.select("#mioGrafico")
 	.append("div")
 	.style("opacity", 0)
-	.attr("class", "tooltip")
+	.attr("class", "nomeBolla")
 	.style("background-color", "gold")
 	.style("border-radius", "5px")
 	.style("padding", "10px")
 	.style("color", "darkred")
 
-var showTooltip = function(d) {
-	tooltip
+var mostraNomeBolla = function(d) {
+	nomeBolla
 		.transition()
 		.duration(200)
 		.style("opacity", 1)
@@ -49,13 +63,13 @@ var showTooltip = function(d) {
 		.style("left", (d3.mouse(this)[0]+30) + "px")
 		.style("top", (d3.mouse(this)[1]+30) + "px")
 }
-var moveTooltip = function(d) {
-	tooltip
+var muoviNomeBolla = function(d) {
+	nomeBolla
 		.style("left", (d3.mouse(this)[0]+30) + "px")
 		.style("top", (d3.mouse(this)[1]+30) + "px")
 }
-var hideTooltip = function(d) {
-	tooltip
+var nascondiNomeBolla = function(d) {
+	nomeBolla
 		.transition()
 		.duration(200)
 		.style("opacity", 0)
@@ -106,10 +120,40 @@ d3.json("data.json")
 			.style("opacity", "0.7")
 			.attr("stroke", "black")
 			.attr("stroke-width", "4")
-			.on("mouseover", showTooltip )
-			.on("mousemove", moveTooltip )
-			.on("mouseleave", hideTooltip )
+			.on("mouseover", mostraNomeBolla)
+			.on("mousemove", muoviNomeBolla)
+			.on("mouseleave", nascondiNomeBolla)
+
+		svg.selectAll('.tick')
+			.on('click', cliccami)
+
+		function cliccami(d) {
+			d3.selectAll("circle").transition().duration(2000).attr("r",d);
+
+			// Aggiorno i valori sugli assi
+			/*var newX = d3.scaleLinear()
+				.domain([0, d3.selectAll('circle').attr("r")])
+				.range([ 0, width ]);
+
+			svg.append("g")
+				.attr("transform", "translate(0," + height + ")")
+				.attr("class","xAxisNew")
+				.call(d3.axisBottom(newX).ticks(20))
+
+
+
+			// Aggiungo l'asse delle Y
+			var newY = d3.scaleLinear()
+				.domain([0, d3.selectAll('circle').attr("r")])
+				.range([ height, 0]);
+			svg.append("g")
+				.attr("class","yAxisNew")
+				.call(d3.axisLeft(newY).ticks(20));*/
+
+
+		}
 	})
+
 	.catch(function(error) {
 		console.log(error); // Se ci sono errori mostrali qui
 	});
